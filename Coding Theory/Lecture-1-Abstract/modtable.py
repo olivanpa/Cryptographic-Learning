bc3481c1c7e125ac1507d5e5dd48c10573a13bf2
LuminolT <copper_sulfate@qq.com>
2022-03-07 21:08:58 +0800

    Configure modtable.py

--- Coding Theory/Lecture-1-Abstract/modtable.py
+++ Coding Theory/Lecture-1-Abstract/modtable.py
@@ -1,13 +1,22 @@
 def get_table(n):
-    print('Mod {} Add'.format(n))
+    print('Mod {} Add\n+ '.format(n), end = '')
     for i in range(n):
+        print('{:3}'.format(i), end = ' ')
+    print('')
+    for i in range(n):
+        print(i, end=' ')
         for j in range(n):
-            print((i + j) % n, end = ' ')
+            print('{:3}'.format((i + j) % n), end = ' ')
         print('')
-    print('Mod {} Multiply'.format(n))
+
+    print('Mod {} Multiply\n* '.format(n), end = '')
     for i in range(n):
+        print('{:3}'.format(i), end = ' ')
+    print('')
+    for i in range(n):
+        print(i, end=' ')
         for j in range(n):
-            print((i * j) % n, end = ' ')
+            print('{:3}'.format((i * j) % n), end = ' ')
         print('')
 
 def main():
